528398da77ba183889091e1ba65bab4d864474e9
Pedro Jorge <skitvandarken2013@gmail.com>
2024-05-28 16:16:12 +0100

    navbar roteado

--- fluxodigitalssr/src/app/layout/navbar/navbar.component.ts
+++ fluxodigitalssr/src/app/layout/navbar/navbar.component.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { Router } from '@angular/router';
+import { InicioComponent } from '../../paginas/inicio/inicio.component';
+import { ServicosComponent } from '../../paginas/servicos/servicos.component';
+import { AcercaComponent } from '../../paginas/acerca/acerca.component';
+import { EmpregosComponent } from '../../paginas/empregos/empregos.component';
+import { ContactoComponent } from '../../paginas/contacto/contacto.component';
 
 @Component({
   selector: 'app-navbar',
@@ -8,6 +13,11 @@ import { Router } from '@angular/router';
   imports: [
 
     RouterModule,
+    InicioComponent,
+    ServicosComponent,
+    AcercaComponent,
+    EmpregosComponent,
+    ContactoComponent
     
 
 
